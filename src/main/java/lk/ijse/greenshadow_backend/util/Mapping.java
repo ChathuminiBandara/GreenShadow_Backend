7dca36b717a08f9fdaa650ded93f05d7e2550d02
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-04 22:18:30 +0530

    Mapper : mapping class for ,modelmapper

---
+++ src/main/java/lk/ijse/greenshadow_backend/util/Mapping.java
@@ -0,0 +1,12 @@
+package lk.ijse.greenshadow_backend.util;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+}
