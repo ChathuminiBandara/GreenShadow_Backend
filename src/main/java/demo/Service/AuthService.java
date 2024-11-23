303e2abbf662cbb3c9017f56d03d56b820071004
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-23 22:35:31 +0530

    auth service

---
+++ src/main/java/demo/Service/AuthService.java
@@ -0,0 +1,12 @@
+package demo.Service;
+
+import com.example.demo.DTO.IMPL.UserDTO;
+import com.example.demo.Secure.JWTAuthResponse;
+import com.example.demo.Secure.SignIn;
+
+
+public interface AuthService {
+    JWTAuthResponse signIn(SignIn signIn);
+    JWTAuthResponse signUp(UserDTO userDTO);
+    JWTAuthResponse refreshToken(String accessToken);
+}
