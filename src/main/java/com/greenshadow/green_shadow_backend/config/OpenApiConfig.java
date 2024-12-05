8ba6d07470f722af8004c3c1d1827000ffbf1022
ChathuminiBandara <chathubandara500@gmail.com>
2024-12-05 17:02:54 +0530

    Open Api Configuration class is done

---
+++ src/main/java/com/greenshadow/green_shadow_backend/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.greenshadow.green_shadow_backend.config;
+
+
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.info.*;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Green Shadow Crop Monitoring API")
+                        .version("1.0")
+                        .description("API documentation for Green Shadow Crop Monitoring System"));
+    }
+}
