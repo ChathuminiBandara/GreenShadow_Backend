3c478329a157c36225f6e438376439d56b48adfd
ChathuminiBandara <chathubandara500@gmail.com>
2024-12-05 22:10:55 +0530

    user details service impl

---
+++ src/main/java/com/greenshadow/green_shadow_backend/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.greenshadow.green_shadow_backend.security.services;
+
+import com.greenshadow.green_shadow_backend.entity.User;
+import com.greenshadow.green_shadow_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
